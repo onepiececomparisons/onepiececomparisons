192b9dded515cb89cf9f50fd25462e31d6681e0b
onepiececomparisons <46995839+onepiececomparisons@users.noreply.github.com>
2019-01-26 11:30:15 +0000

    test

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
+
 function swapImg(image, manga_image, anime_image, scroll, width) {
-    if (image.src != manga_image) {
+    var current = image.src;
+    current_name = current.split('/').reverse()[0];
+    manga_image_name = manga_image.split('/').reverse()[0];
+    anime_image_name = anime_image.split('/').reverse()[0];
+    
+    if (current_name !=manga_image_name) {
         image.src = manga_image;
         image.style.width = width;
-        if (scroll == 1){
+        if (scroll == 1) {
             window.scrollBy(0, window.innerHeight);
         }
         if (scroll == 0)
-            window.scrollBy(0, window.innerHeight/7);
-    } else{
+            window.scrollBy(0, window.innerHeight / 7);
+    } else {
         image.src = anime_image;
-        window.scrollTo(0,0);
+        window.scrollTo(0, 0);
     }
+    window.scrollBy(0, window.innerHeight);
+        $('#img').text(scroll);
 
 }
-
